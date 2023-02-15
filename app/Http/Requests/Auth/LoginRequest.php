48b8b59eceb10aa7cfefb6e2606b03632b6b88b6
Rafael Arend <rafinhaarend123@hotmail.com>
2023-02-15 03:42:52 -0400

    Impede de usuário inativado efetuar login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,14 +41,19 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            if (Auth::user()->status != \App\Enums\Status::ACTIVE) {
+                Auth::logout();
+                throw ValidationException::withMessages([
+                    'email' => __('Usuário inativado!'),
+                ]);
+            }
+        } else {
             RateLimiter::hit($this->throttleKey());
-
             throw ValidationException::withMessages([
                 'email' => __('auth.failed'),
             ]);
         }
-
         RateLimiter::clear($this->throttleKey());
     }
 
@@ -59,7 +64,7 @@ class LoginRequest extends FormRequest
      */
     public function ensureIsNotRateLimited(): void
     {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
 
@@ -80,6 +85,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->input('email')) . '|' . $this->ip());
     }
 }
